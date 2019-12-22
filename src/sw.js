ce38c9172908c720619cfc1ab3fddb5c0b665ff6
paczulapiotr <paczulapiotr@gmail.com>
2019-12-22 21:20:42 +0100

    Add missing asset caching

--- src/sw.js
+++ src/sw.js
@@ -1,14 +1,16 @@
-const staticCacheName = "site-static-v1.1.4";
+const staticCacheName = "site-static-v1.2.0";
 const assets = [
-  "./",
-  "./index.html",
-  "./main.bundle.js",
-  "./manifest.json",
-  "./public/icons/icon_72x72.png",
-  "./public/icons/icon_96x96.png",
-  "./public/icons/icon_128x128.png",
-  "./public/icons/icon_256x256.png",
-  "./public/icons/icon_512x512.png"
+  "/",
+  "/index.html",
+  "/main.bundle.js",
+  "/manifest.json",
+  "/public/icons/icon_72x72.png",
+  "/public/icons/icon_96x96.png",
+  "/public/icons/icon_128x128.png",
+  "/public/icons/icon_256x256.png",
+  "/public/icons/icon_512x512.png",
+  "https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap",
+  "https://fonts.gstatic.com/s/roboto/v20/KFOlCnqEu92Fr1MmWUlfBBc4AMP6lQ.woff2"
 ];
 
 // install service worker
@@ -32,5 +34,14 @@ self.addEventListener("activate", e => {
             .map(key => caches.delete(key))
         )
       )
+  );
+});
+
+// fetch event
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(cacheResp => {
+      return cacheResp || fetch(e.request);
+    })
   );
 });
